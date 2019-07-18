f0ed4097d00dc1801fefe01accf832b7a89c37f9
nao_kanazawa <nao_kanazawa@radix.ad.jp>
2019-07-18 10:03:56 +0900

    button押下がchangeDetectionのタイミングの一つ

---
+++ src/app/sample/sample.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sample',
+  templateUrl: './sample.component.html',
+  styleUrls: ['./sample.component.css']
+})
+export class SampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  get time() {
+    return Date.now();
+  }
+}
